14f0e54e9d7609ab585a3b19a62a49cf05390e04
Bresenham <gaul@hm.edu>
2018-03-29 13:38:45 +0100

    UART does almost work

--- CProgs/registers.h
+++ CProgs/registers.h
@@ -21,14 +21,28 @@
 #define TIMER_32BIT_MODE    1
 
 /* MINI-UART ADDRESSES */
+#define AUX_EN_REG          0x20215004
 #define AUX_MU_LCR_REG      0x2021504C
 #define AUX_MU_BAUD         0x20215068
 #define AUX_MU_CNTL_REG     0x20215060
+#define AUX_MU_STAT_REG     0x20215064
+#define AUX_MU_BAUD_REG     0x20215068
+#define AUX_MU_IO_REG       0x20215040
+#define AUX_MU_LSR_REG      0x20215054
+#define AUX_MU_IER_REG      0x20215044
+#define AUX_MU_IIR_REG      0x20215048
+#define AUX_MU_MCR_REG      0x20215050
 
 /* MINI-UART BITS */
 #define AUX_MU_LCR_REG_8BIT_MODE        0
+#define AUX_MU_LCR_REG_DLAB             7
 #define AUX_MU_CNTL_REG_ENABLE_TRANS    1
 #define AUX_MU_CNTL_REG_ENABLE_RECV     0
+#define AUX_MU_STAT_REG_TRANS_SPACE     1
+#define AUX_MU_STAT_REG_RECV_SPACE      0
+#define AUX_EN_REG_ENABLE               0
+#define AUX_MU_LSR_REG_TRANS_EMPTY      5
+#define AUX_MU_LSR_REG_TRANS_IDLE       6
 
 /* GPIO ADDRESSES */
 #define GPIO_BASE       0x20200000
@@ -48,11 +62,20 @@
 #define GPCLR0          0x20200028
 #define GPCLR1          0x2020002C
 
+/* GPIO ADDRESSES - GPPUD */
+#define GPPUD           0x20200094
+#define GPPUDCLK0       0x20200098
+
 /* GPIO BITS */
-#define GPIO2_GPFSEL_OUT       6
-#define GPIO2                  2
+#define GPPUDCLK0_LINE14_CLK    14
+#define GPIO2_GPFSEL_OUT        6
+#define GPIO2                   2
+
 #define GPIO14_GPFSEL_ALT0      14
 #define GPIO15_GPFSEL_ALT0      17
+
+#define GPIO14_GPFSEL_ALT5      13
+#define GPIO15_GPFSEL_ALT5      16
 
 #define GPIO47_GPFSEL_OUT       21
 #define GPIO47                  15
--- CProgs/test.c
+++ CProgs/test.c
@@ -1,5 +1,8 @@
-#include "gpio_conf.h"
+#include "registers.h"
 #include "stdbool.h"
+#include "string.h"
+
+void writeString(char *c);
 
 static volatile bool led_on = false;
 
@@ -35,13 +38,50 @@ __attribute__ ((interrupt ("ABORT"))) void interrupt_prefetch_abort(void){}
 __attribute__ ((interrupt ("IRQ"))) void interrupt_irq(void)
 {
     if(GET(TIMER_READ_IRQ)){
-        if(led_on)
+        if(led_on) {
             SET(GPCLR1, 1 << GPIO47);
-        else
+            writeString("LED IS ON\n");
+        }
+        else {
             SET(GPSET1, 1 << GPIO47);
+            writeString("LED IS OFFF\n");
+        }
         led_on = !led_on;
         SET(TIMER_CLEAR_IRQ, 0);
     }
+}
+
+void writeString(char *str) {
+    while(*str) {
+        if((GET(AUX_MU_LSR_REG) & (1 << AUX_MU_LSR_REG_TRANS_EMPTY)));
+            SET(AUX_MU_IO_REG,*str++);
+    }
+    //while(!(GET(AUX_MU_LSR_REG) & (1 << AUX_MU_LSR_REG_TRANS_IDLE)));
+}
+
+/*
+ * ((250,000,000/460800)/8)-1 = 67 
+ */ 
+void initUART() {
+    int ra;
+    SET(AUX_EN_REG, 1 << AUX_EN_REG_ENABLE);
+    SET(AUX_MU_IER_REG, 0);
+    SET(AUX_MU_CNTL_REG, 0);
+    SET(AUX_MU_LCR_REG, 3 << AUX_MU_LCR_REG_8BIT_MODE);
+    SET(AUX_MU_MCR_REG, 0);
+    SET(AUX_MU_IER_REG, 0);
+    SET(AUX_MU_IIR_REG, 0xC6);
+    SET(AUX_MU_BAUD_REG, 67);
+    
+    SET(GPFSEL1, (1 << GPIO14_GPFSEL_ALT5) | (1 << GPIO15_GPFSEL_ALT5));
+    
+    SET(GPPUD,0);
+    for(ra=0;ra<150;ra++) asm volatile ("nop");
+    SET(GPPUDCLK0,(1<<GPPUDCLK0_LINE14_CLK));
+    for(ra=0;ra<150;ra++) asm volatile ("nop");
+    SET(GPPUDCLK0,0);
+    
+    SET(AUX_MU_CNTL_REG, 1 << AUX_MU_CNTL_REG_ENABLE_TRANS);
 }
 
 int main(void)
@@ -53,7 +93,9 @@ int main(void)
     SET(INTR_ENABLE_BASE, 1 << INTR_ENABLE_BASE_TIMER_IRQ);
     SET(TIMER_LOAD, 0x00020000);
     SET(TIMER_CTRL, (1 << TIMER_32BIT_MODE) | (1 << TIMER_IRQ_ENABLE) | (1 << TIMER_ENABLE));
-
+    
+    initUART();
+    
     while(1)
         ;
 }
